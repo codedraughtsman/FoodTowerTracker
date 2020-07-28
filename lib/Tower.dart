d8b8a9cb3f0adfe2355bc7ceef0ada6b10397915
codedraughtsman <codedraughtsman@users.noreply.github.com>
2020-07-28 12:43:05 +1200

    got the tower to resize properly

--- lib/Tower.dart
+++ lib/Tower.dart
@@ -53,13 +53,19 @@ class _TowerState extends State<Tower> {
           }
           return Container(
 //            color: Colors.cyan,
-            child: Column(
-              children: _towerBlocks(
-                context,
-                snapshot,
-              ),
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                xSize = constraints.maxWidth;
+                ySize = constraints.maxWidth;
+                return Column(
+                  children: _towerBlocks(
+                    context,
+                    snapshot,
+                  ),
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                );
+              },
             ),
           );
         } else {
@@ -71,11 +77,94 @@ class _TowerState extends State<Tower> {
   }
 
   _towerBlocks(BuildContext context, AsyncSnapshot snapshot) {
+    var portions = <PortionEntry>[];
+
+    for (var s in snapshot.data) {
+      portions.add(PortionEntry.fromMap(s));
+    }
+
+    //sort portions by max y value
+    portions.sort((a, b) => a
+        .getTotalValueInMeasure(towerTypeString)
+        .compareTo(b.getTotalValueInMeasure(towerTypeString)));
+
+    double maxXValue = 0.0;
+    portions.forEach((PortionEntry portion) {
+      double value = portion.getMappedValue(towerTypeString) *
+          100 /
+          portion.getMappedValue("measure");
+      maxXValue = math.max(value, maxXValue);
+    });
+
+    var towerBlocks = List<Widget>();
+    portions.forEach((PortionEntry portion) {
+      double valuePer100g = portion.getMappedValue(towerTypeString) *
+          100 /
+          portion.getMappedValue("measure");
+
+      towerBlocks.add(
+        Expanded(
+          flex: portion.getTotalValueInMeasure(towerTypeString).toInt(),
+          child: Stack(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: valuePer100g.toInt(),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: Colors.primaries[
+                            portion.foodId % Colors.primaries.length],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: (maxXValue - valuePer100g).toInt(),
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+
+//                direction: Axis.vertical,
+                children: <Widget>[
+                  Container(
+//                    color: Colors.blueGrey,
+                    child: Text(
+                      portion.name,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+//                  Expanded(
+////                    flex: maxXValue.toInt(),
+//                    child: Text(
+//                      portion.name,
+//                      overflow: TextOverflow.ellipsis,
+//                      textAlign: TextAlign.end,
+//                    ),
+//                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+
+    return towerBlocks;
+  }
+
+  _towerBlocks_old(BuildContext context, AsyncSnapshot snapshot) {
     double totalYValue = 0.0;
     var portions = <PortionEntry>[];
 
     for (var s in snapshot.data) {
-      //log(s);
       portions.add(PortionEntry.fromMap(s));
     }
 
